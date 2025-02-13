d0ba82eb2adb89190f693b57610fc082f3e2ce24
Ccilpy <cecilepuyol@gmail.com>
2025-02-13 11:11:57 +0100

    add modules and routes

--- server/src/router.ts
+++ server/src/router.ts
@@ -15,4 +15,12 @@ router.post("/api/items", itemActions.add);
 
 /* ************************************************************************* */
 
+import cardActions from "./modules/card/cardActions";
+
+router.get("/api/cards", cardActions.browse);
+router.get("/api/cards/:id", cardActions.read);
+router.post("/api/cards", cardActions.add);
+router.put("/api/cards/:id", cardActions.edit);
+router.delete("/api/cards/:id", cardActions.destroy);
+
 export default router;
